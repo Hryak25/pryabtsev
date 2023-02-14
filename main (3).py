33de62f2c70725a278ff19960857cc0ae435faee
Hryak25 <125359274+Hryak25@users.noreply.github.com>
2023-02-14 14:22:58 +0300

    пр 1 14.02.2023

---
+++ main (3).py
@@ -0,0 +1,33 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+print("Hello World")
+tseloe = 10
+drobnoe = 8.4 
+stroka ='No'
+stroka2 ='yes'
+stroka3 ='_'
+big_netseloe = tseloe * 3.5
+drobnoe = drobnoe - 1 
+stroka = stroka * 2 
+stroka2 = stroka2 * 3 
+stroka = stroka + stroka3 + stroka2
+print (tseloe / drobnoe)
+print (big_netseloe / drobnoe)
+print (tseloe)
+print (drobnoe) 
+print (stroka)
+print (big_netseloe)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
