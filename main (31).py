0f1ddc70647905fe8664eeedea5a17e25fde25ff
Hryak25 <125359274+Hryak25@users.noreply.github.com>
2023-05-15 15:36:33 +0300

    лаба 15.05.23

---
+++ main (31).py
@@ -0,0 +1,15 @@
+def schet ():
+    a= int(input ("введите первое число"))
+    b=int (input("введите второе число"))
+    c=int(input("введите третье число"))
+    if a>b:
+        print (a-b)
+    elif c>b:
+          print (c-b)
+    else: 
+        print(b-a)
+    return a, b, c
+k, d, j = schet()
+e, f, s = schet()
+z, x, w =schet()        
+schet()
